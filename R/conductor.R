4d4a794d7282b1ebecd2ecd73fdb6ad83f523a03
etiennebacher <you@example.com>
2022-04-11 15:25:49 +0200

    docs: start documentation for R6 methods

--- R/conductor.R
+++ R/conductor.R
@@ -15,6 +15,9 @@ useConductor <- function(){
   )
 }
 
+
+#' Create a "conductor" tour
+#'
 #' @export
 Conductor <- R6::R6Class(
   "Conductor",
@@ -24,7 +27,33 @@ Conductor <- R6::R6Class(
     globals = list(),
     mathjax = FALSE
   ),
+
+
+  #' @details
+  #' Create a new `Conductor` object.
+  #'
+  #'
+  #' @return A `Conductor` object.
   public = list(
+
+    #' @param exitOnEsc Allow closing the tour by pressing "Escape". Default is `TRUE`.
+    #' @param keyboardNavigation Allow navigating the tour with keyboard arrows. Default is `TRUE`.
+    #' @param useModalOverlay Highlight the tour popover and the element (if specified). Default is `TRUE`.
+    #' @param classPrefix Add a prefix to the classes of the tour. This allows having different CSS for each tour.
+    #' @param tourName An (optional) id to give to the tour.
+    #' @param stepsContainer
+    #' @param modalContainer
+    #' @param confirmCancel Ask confirmation to cancel the tour. Default is `FALSE`.
+    #' @param confirmCancelMessage Message in the popup that ask confirmation to close the tour (only works if `confirmCancel = TRUE`).
+    #' @param defaultStepOptions A nested list of options to apply to the entire tour. See `Details`.
+    #' @param mathjax Enable MathJax? Default is `FALSE`. This requires importing MathJax, for example with `shiny::withMathJax()`.
+    #' @param onComplete A JavaScript code to run when the tour is completed.
+    #' @param onCancel A JavaScript code to run when the tour is cancelled
+    #' @param onHide A JavaScript code to run when the tour is hidden
+    #' @param onShow A JavaScript code to run when the tour is shown.
+    #' @param onStart A JavaScript code to run when the tour starts.
+    #' @param onActive A JavaScript code to run when the tour is active.
+    #' @param onInactive A JavaScript code to run when the tour is inactive.
 
     initialize = function(exitOnEsc = TRUE, keyboardNavigation = TRUE,
                           useModalOverlay = TRUE, classPrefix = NULL,
@@ -60,6 +89,11 @@ Conductor <- R6::R6Class(
       invisible(self)
     },
 
+
+    #' @param session A valid Shiny session. If `NULL` (default), the function attempts to get the session with `shiny::getDefaultReactiveDomain()`.
+    #'
+    #' @details
+    #' Initialise `Conductor`.
     init = function(session = NULL) {
       if(is.null(session)) {
         session <- shiny::getDefaultReactiveDomain()
@@ -76,6 +110,11 @@ Conductor <- R6::R6Class(
       invisible(self)
     },
 
+
+    #' @param session A valid Shiny session. If `NULL` (default), the function attempts to get the session with `shiny::getDefaultReactiveDomain()`.
+    #'
+    #' @details
+    #' Start `Conductor`.
     start = function(session = NULL) {
       if(is.null(session)) {
         session <- shiny::getDefaultReactiveDomain()
@@ -83,6 +122,24 @@ Conductor <- R6::R6Class(
       session$sendCustomMessage("conductor-start", list(id = private$id))
       invisible(self)
     },
+
+
+    #' Add a step in a `Conductor` tour
+    #'
+    #' @param el The id of the element to highlight. If `NULL` (default), the popover will appear in the center of the page.
+    #' @param title Title of the popover.
+    #' @param text Text of the popover.
+    #' @param position Position of the popover relative to the element. Possible values are: 'auto', 'auto-start', 'auto-end', 'top', 'top-start', 'top-end', 'bottom', 'bottom-start', 'bottom-end', 'right', 'right-start', 'right-end', 'left', 'left-start', 'left-end'.
+    #' @param arrow Add an arrow pointing towards the highlighted element? Default is `TRUE`.
+    #' @param is_id
+    #' @param canClickTarget Allow the highlighted element to be clicked? Default is `TRUE`.
+    #' @param advanceOn
+    #' @param scrollTo
+    #' @param cancelIcon A list of two elements: `enabled` is a boolean indicating whether a "close" icon should be displayed (default is `TRUE`); `label` is the label to add for `aria-label`.
+    #' @param when
+    #' @param showOn Either a boolean or a JavaScript expression that returns `true` or `false`. It indicates whether the step should be displayed in the tour.
+    #'
+    #' @details
 
     step = function(el = NULL, title = NULL, text = NULL, position = NULL,
                     arrow = TRUE, is_id = TRUE, canClickTarget = TRUE,
@@ -162,10 +219,6 @@ Conductor <- R6::R6Class(
       }
       session$sendCustomMessage("conductor-isActive", list(id = private$id))
       invisible(self)
-    },
-
-    print = function() {
-      print(private$steps)
     }
 
   )
